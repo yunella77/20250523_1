348144cecf8782bd1ef8c67074d7e183b9a43eff
yunella77 <413730374@o365.tku.edu.tw>
2025-05-23 11:06:50 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,64 +1,39 @@
-let video;
 let facemesh;
+let video;
 let predictions = [];
-
-// 紅色線的點
-const redIndices = [
-  409,270,269,267,0,37,39,40,185,61,146,91,181,84,17,
-  314,405,321,375,291,76,77,90,180,85,16,315,404,320,
-  307,306,408,304,303,302,11,72,73,74,184
-];
-
-// 藍色線的點
-const blueIndices = [
-  243,190,56,28,27,29,30,247,130,25,110,24,23,22,26,
-  112,133,173,157,158,159,160,161,246,33,7,163,144,145,153,154,155
-];
+const indices = [409, 270, 269, 267, 0, 37, 39, 40, 185, 61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291, 76, 77, 90, 180, 85, 16, 315, 404, 320, 307, 306, 408, 304, 303, 302, 11, 72, 73, 74, 184];
 
 function setup() {
-  createCanvas(640, 480).position(
-    (windowWidth - 640) / 2,
-    (windowHeight - 480) / 2
-  );
+  createCanvas(640, 480).parent(document.body);
+  // 將畫布置中
+  let cnv = createCanvas(640, 480);
+  cnv.style('display', 'block');
+  cnv.position((windowWidth - width) / 2, (windowHeight - height) / 2);
+
   video = createCapture(VIDEO);
   video.size(width, height);
   video.hide();
 
   facemesh = ml5.facemesh(video, modelReady);
-  facemesh.on('predict', results => {
-    predictions = results;
-  });
+  facemesh.on('predict', results => predictions = results);
 }
 
 function modelReady() {
-  console.log('Facemesh model loaded!');
+  console.log('FaceMesh model loaded!');
 }
 
 function draw() {
+  background(220);
   image(video, 0, 0, width, height);
 
   if (predictions.length > 0) {
     const keypoints = predictions[0].scaledMesh;
-
-    // 畫紅色線
     stroke(255, 0, 0);
     strokeWeight(15);
     noFill();
     beginShape();
-    for (let i = 0; i < redIndices.length; i++) {
-      const idx = redIndices[i];
-      const [x, y] = keypoints[idx];
-      vertex(x, y);
-    }
-    endShape();
-
-    // 畫藍色線
-    stroke(0, 0, 255);
-    strokeWeight(15);
-    noFill();
-    beginShape();
-    for (let i = 0; i < blueIndices.length; i++) {
-      const idx = blueIndices[i];
+    for (let i = 0; i < indices.length; i++) {
+      const idx = indices[i];
       const [x, y] = keypoints[idx];
       vertex(x, y);
     }
